bf4d1022585432887bce0bdc57bff45d8481a747
David Hernandez <dae.aznar@gmail.com>
2020-12-02 17:24:12 -0600

    Bressenham's Algohorithm

--- src/test.py
+++ src/test.py
@@ -1,7 +1,8 @@
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
-import sys
+
+w, h = 500, 500
 
 
 def linebress(x1, y1, x2, y2):
@@ -143,15 +144,56 @@ def linebress(x1, y1, x2, y2):
         print(y)
 
 
-def main():
-    xi = int(input("Ingresa Xi: "))
-    yi = int(input("Ingresa Yi: "))
-    xf = int(input("Ingresa Xf: "))
-    yf = int(input("Ingresa Yf: "))
+# ---Section 1---
+def square():
+    # We have to declare the points in this sequence: bottom left, bottom right, top right, top left
+    glBegin(GL_QUADS)  # Begin the sketch
+    glVertex2f(10, 10)  # Coordinates for the bottom left point
+    glVertex2f(20, 10)  # Coordinates for the bottom right point
+    glVertex2f(20, 20)  # Coordinates for the top right point
+    glVertex2f(10, 20)  # Coordinates for the top left point
+    glEnd()  # Mark the end of drawing
 
-    linebress(xi, yi, xf, yf)
+def square2():
+    # We have to declare the points in this sequence: bottom left, bottom right, top right, top left
+    glBegin(GL_QUADS)  # Begin the sketch
+    glVertex2f(100, 100)  # Coordinates for the bottom left point
+    glVertex2f(200, 100)  # Coordinates for the bottom right point
+    glVertex2f(200, 200)  # Coordinates for the top right point
+    glVertex2f(100, 200)  # Coordinates for the top left point
+    glEnd()  # Mark the end of drawing
 
 
+def iterate():
+    glViewport(0, 0, 500, 500)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(0.0, 500, 0.0, 500, 0.0, 1.0)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
 
-if __name__ == '__main__':
-    main()
+
+# ---Section 2---
+
+def showScreen():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)  # Remove everything from screen (i.e. displays all white)
+    glLoadIdentity()  # Reset all graphic/shape's position
+    iterate()
+    glColor3f(1.0, 0.0, 3.0)  # Set the color to pink
+    square()
+    square2()# Draw a square using our function
+    glutSwapBuffers()
+
+
+# ---Section 3---
+
+x1= int(input("Dame x1: "))
+y1= int(input("Dame y1: "))
+glutInit()
+glutInitDisplayMode(GLUT_RGBA)  # Set the display mode to be colored
+glutInitWindowSize(500, 500)  # Set the w and h of your window
+glutInitWindowPosition(0, 0)  # Set the position at which this windows should appear
+wind = glutCreateWindow("OpenGL Square")  # Set a window title
+glutDisplayFunc(showScreen)
+glutIdleFunc(showScreen)  # Keeps the window open
+glutMainLoop()  # Keeps the above created window displaying/running in a loop
